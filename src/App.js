34e799d808a89562e8f2d07a7a218b100a9461f8
luizg <luiz.g.f.s@hotmail.com>
2025-03-16 00:33:50 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div>
 
-      {/* Defina as rotas */}
       <Routes>
         <Route path="/" element={<Navigate to="/mesasom" />} />
         <Route path="/photos" element={<Photos />} />
